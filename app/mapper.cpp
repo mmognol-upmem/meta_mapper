965e2f2487bc10883eb34a409b74f92074083ff1
Meven MOGNOL <mmognol@upmem.com>
2024-12-16 19:14:13 +0100

    Refactor mapper and command parsing; integrate bloom filter file handling and enforce required arguments

--- app/mapper.cpp
+++ app/mapper.cpp
@@ -1,21 +1,22 @@
 #include "bloom_filter.hpp"
+#include "file_utils.hpp"
 #include "parse_command.hpp"
+#include "read_mapper.hpp"
 
 int main(int argc, char *argv[])
 {
     auto parsed = parse_mapper(argc, argv);
 
-    // Create a new BloomFilter object
+    auto reference_file = validate_reference_file(parsed["reference"].as<std::string>());
+    auto nb_ranks = parsed["ranks"].as<ssize_t>();
+    auto nb_dpu = nb_ranks * 64;
+
+    auto bloom_file = generate_bloom_file_path(reference_file, nb_dpu, HASH_SIZE);
+
     MultiBloomFilter bf{};
-    // Initialize the BloomFilter object with 1 filter and size2 of 10
-    bf.initialize(1, 10);
-    // Insert a hash into the BloomFilter object
-    bf.insert(0, 123456);
-    // Save the BloomFilter object to a file
-    bf.save_to_file("bloom_filter.bin");
-    // Load the BloomFilter object from a file
-    bf.load_from_file("bloom_filter.bin");
-    // Get the size2 of the BloomFilter object
+    bf.load_from_file(bloom_file);
+
+    printf("Bloom filter size: %lu\n", bf.get_size2());
 
     return 0;
 }
--- src/parse_command.cpp
+++ src/parse_command.cpp
@@ -10,12 +10,11 @@ cxxopts::ParseResult parse_mapper(int argc, char *argv[])
         "r,reference", "URI to reference genome (e.g. 'file://genome.fa' or 'file://genome_album.txt')",
         cxxopts::value<std::string>())("U,queries", "Path to queries file", cxxopts::value<std::string>())(
         "k,ranks", "Number of PIM ranks", cxxopts::value<ssize_t>()->default_value("4"))(
-        "b,bloom", "Force reconstruction of Bloom filter", cxxopts::value<bool>()->default_value("false"))(
         "s,sam", "Path of output in SAM format", cxxopts::value<bool>()->default_value("false"))("h,help", "Print usage");
 
     auto result = options.parse(argc, argv);
 
-    if (result.count("help") > 0 || result.arguments().empty())
+    if (result.count("help") > 0 || result.count("reference") == 0 || result.count("ranks") == 0 || result.count("sam") == 0)
         exit(printf("%s\n", options.help().c_str()));
 
     return result;
