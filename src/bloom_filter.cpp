8c74026d62919dcdfcd16dd2852c8997fccbe170
Meven MOGNOL <mmognol@upmem.com>
2024-12-18 14:21:47 +0100

    Refactor BloomFilter implementation to enhance lazy evaluation and improve packing logic

--- src/bloom_filter.cpp
+++ src/bloom_filter.cpp
@@ -1,6 +1,16 @@
 #include "bloom_filter.hpp"
 
 /* -------------------------------------------------------------------------- */
+/*                              filter static data                            */
+/* -------------------------------------------------------------------------- */
+
+constexpr uint64_t BSF_DEBRUIJN64 = 0x03f79d71b4cb0a89UL;
+constexpr int BSF_INDEX64[64] = {0, 1, 48, 2, 57, 49, 28, 3, 61, 58, 50, 42, 38, 29, 17, 4,
+                                 62, 55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5,
+                                 63, 47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11,
+                                 46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6};
+
+/* -------------------------------------------------------------------------- */
 /*                              utils functions                               */
 /* -------------------------------------------------------------------------- */
 
@@ -22,6 +32,33 @@ void read(T &data, std::ifstream &in)
         in.read(reinterpret_cast<char *>(&data), sizeof(data));
 }
 
+inline uint64_t blsr_u64(uint64_t val)
+{
+    return val & (val - 1);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 Lazy result                                */
+/* -------------------------------------------------------------------------- */
+
+void ResultIterator::next()
+{
+    for (; m_i >= 0; --m_i)
+    {
+        if (val == 0)
+        {
+            val = m_filter[m_h[0] * m_sub_size + m_i - 1] &
+                  m_filter[m_h[1] * m_sub_size + m_i - 1];
+        }
+        else
+        {
+            result = (m_i << bf_pack_size2) + BSF_INDEX64[((val & -val) * BSF_DEBRUIJN64) >> 58];
+            val = blsr_u64(val);
+            return;
+        }
+    }
+}
+
 /* -------------------------------------------------------------------------- */
 /*                          MultiBloomFilter implem                           */
 /* -------------------------------------------------------------------------- */
@@ -32,20 +69,25 @@ void MultiBloomFilter::initialize(const ssize_t nb_filters, const ssize_t size2)
     m_size2 = size2;
     m_size = 1L << m_size2;
     m_size_reduced = m_size - 1;
-    m_sub_size = (m_nb_filters + m_pack_size - 1) >> m_pack_size2;
+    m_sub_size = (m_nb_filters + bf_pack_size - 1) >> bf_pack_size2;
     m_data.resize(m_size * m_sub_size, static_cast<pack_t>(0));
 }
 
 void MultiBloomFilter::insert(const size_t idx, const hash_t hash)
 {
-    auto i = idx >> m_pack_size2;
+    auto i = idx >> bf_pack_size2;
     auto h = hash & m_size_reduced;
     auto current_val = __sync_fetch_and_or(m_data.data() + h * m_sub_size + i, 0);
     if (__builtin_popcountll(current_val) < m_MAX_SET_BITS_PER_PACKED_VALUE)
     {
-        auto mask = m_bit_mask[idx & (m_pack_size - 1)];
+        auto mask = m_bit_mask[idx & (bf_pack_size - 1)];
         __sync_fetch_and_or(m_data.data() + h * m_sub_size + i, mask);
     }
+}
+
+LazyBfResult MultiBloomFilter::contains(const hash_t hash1, const hash_t hash2)
+{
+    return LazyBfResult({hash1 & m_size_reduced, hash2 & m_size_reduced}, m_data.data(), m_sub_size);
 }
 
 void MultiBloomFilter::save_to_file(std::string file_path) const
@@ -76,9 +118,9 @@ void MultiBloomFilter::load_from_file(std::string file_path)
  */
 std::pair<uint64_t, uint64_t> MultiBloomFilter::place_mask(const size_t idx, const hash_t hash) const
 {
-    auto i = idx >> m_pack_size2;
+    auto i = idx >> bf_pack_size2;
     auto h = hash & m_size_reduced;
-    return {h * m_sub_size + i, m_bit_mask[idx & (m_pack_size - 1)]};
+    return {h * m_sub_size + i, m_bit_mask[idx & (bf_pack_size - 1)]};
 }
 
 void MultiBloomFilter::insert_computed(const uint64_t place, const uint64_t mask)
--- src/bloom_filter.hpp
+++ src/bloom_filter.hpp
@@ -14,6 +14,10 @@
 
 constexpr uint64_t PMASK32 = (1UL << 32) - 1;
 using hash_t = uint32_t;
+using pack_t = uint64_t;
+
+constexpr ssize_t bf_pack_size2 = 6;
+constexpr ssize_t bf_pack_size = 1 << bf_pack_size2;
 
 template <typename T, size_t S>
 consteval auto generate_bit_mask()
@@ -27,13 +31,58 @@ consteval auto generate_bit_mask()
 }
 
 /* -------------------------------------------------------------------------- */
+/*                                 Lazy result                                */
+/* -------------------------------------------------------------------------- */
+
+class ResultIterator
+{
+public:
+    ResultIterator() = default;
+
+    ResultIterator(int i, std::array<uint64_t, 2> h, pack_t *filter)
+        : m_i(i), m_sub_size(i), m_h(h), m_filter(filter)
+    {
+        next();
+    }
+    bool operator!=(const ResultIterator &other) const { return m_i != other.m_i; }
+    ResultIterator &operator++()
+    {
+        next();
+        return *this;
+    }
+    size_t operator*() const { return result; }
+    void next();
+
+private:
+    int m_i{-1};
+    int m_sub_size{};
+    std::array<uint64_t, 2> m_h{};
+    pack_t *m_filter{};
+    size_t result{};
+    uint64_t val{};
+};
+
+class LazyBfResult
+{
+public:
+    LazyBfResult(std::array<uint64_t, 2> h, pack_t *filter, ssize_t sub_size) : m_h(h), m_filter(filter), m_sub_size(sub_size) {}
+    auto begin() { return ResultIterator(static_cast<int>(m_sub_size), m_h, m_filter); }
+    static auto end() { return ResultIterator{}; }
+
+private:
+    std::array<uint64_t, 2> m_h;
+    pack_t *m_filter;
+    ssize_t m_sub_size;
+};
+;
+
+/* -------------------------------------------------------------------------- */
 /*                                 BloomFilter                                */
 /* -------------------------------------------------------------------------- */
 
 class MultiBloomFilter
 {
-private:
-    using pack_t = uint64_t;
+    friend class LazyBfResult;
 
 public:
     MultiBloomFilter() {}
@@ -47,6 +96,9 @@ public:
     /// @param idx idx of the filter to insert in
     /// @param hash hashed item to insert
     void insert(const size_t idx, const hash_t hash);
+    LazyBfResult contains(const hash_t hash1, const hash_t hash2);
+    auto &data() { return m_data; }
+    const auto &data() const { return m_data; }
 
     void prefetch(const hash_t hash);
 
@@ -64,23 +116,20 @@ public:
     /// @brief Return size2 of the filters
     /// @return size2
     size_t get_size2() const { return m_size2; }
+    ssize_t sub_size() const { return m_sub_size; }
 
     double weight() const;
 
 private:
-    static constexpr ssize_t m_pack_size2 = 6;
-
-    static constexpr ssize_t m_pack_size = 1 << m_pack_size2;
-
     ssize_t m_nb_filters{};
     ssize_t m_size2{};
     ssize_t m_size{};
-    ssize_t m_size_reduced{};
+    size_t m_size_reduced{};
     ssize_t m_sub_size{};
 
     std::vector<pack_t> m_data;
 
-    static constexpr auto m_bit_mask = generate_bit_mask<pack_t, m_pack_size>();
+    static constexpr auto m_bit_mask = generate_bit_mask<pack_t, bf_pack_size>();
     static constexpr size_t m_CONTAINS_RESERVE = 8;
     static constexpr size_t m_BLOCK_SIZE2 = 6;
     static constexpr size_t m_BLOCK_SIZE = (1 << m_BLOCK_SIZE2);
