1ba857afdef44c4f22edb0438893355cb56054aa
Meven MOGNOL <mmognol@upmem.com>
2024-12-18 14:22:05 +0100

    Refactor DpuMapper to improve query dispatching and enhance internal data structures

--- src/dpu_mapper.cpp
+++ src/dpu_mapper.cpp
@@ -2,9 +2,13 @@
 
 #include "dpu_mapper.hpp"
 #include "file_utils.hpp"
-#include "read_mapper.hpp"
+#include "signature.hpp"
 
 #include "dpu_mapper_helper.hpp"
+
+/* -------------------------------------------------------------------------- */
+/*                               utils functions                              */
+/* -------------------------------------------------------------------------- */
 
 ssize_t compute_dpu_reference_size(size_t reference_size, ssize_t nb_dpu, ssize_t overlap)
 {
@@ -41,119 +45,7 @@ MultiBloomFilter get_bloom_filter(const std::string &reference_file, const Compa
         printf("Bloom filter size: %lu\n", bf.get_size2());
         return bf;
     }
-    else
-        return load_bloom_filter(reference_file, nb_dpu, HASH_SIZE);
-}
-
-size_t dispatch_query(const Read &query, size_t shift, MappingWorkerData &mapping_data)
-{
-    size_t nb_dispatches = 0;
-    auto start_pos = find_good_pos(query, get_round_shift(round_idx, query.seq.size()));
-    if constexpr (DISPATCH_TO_ALL)
-    {
-        for (size_t dpu_id = 0; dpu_id < _nb_dpu; ++dpu_id)
-        {
-            dispatch_query_to_dpu(dpu_id, query, start_pos, mapping_data);
-            ++nb_dispatches;
-        }
-    }
-    else
-    {
-        auto start_pos2 = find_good_pos(query, start_pos + m_seed_search.delta);
-        auto signatures = Signature<HASH_SIZE>::hash(query, start_pos, start_pos2);
-        for (auto dpu_id : _bloom_filters.contains(signatures.first, signatures.second))
-        {
-            dispatch_query_to_dpu(dpu_id, query, start_pos, mapping_data);
-            ++nb_dispatches;
-        }
-    }
-
-    return nb_dispatches;
-}
-
-/* -------------------------------------------------------------------------- */
-/*                               DpuMapper implem                             */
-/* -------------------------------------------------------------------------- */
-
-DpuMapper::DpuMapper(const std::string &reference_path, ssize_t nb_ranks, bool create_bf) : m_rankset(nb_ranks, nb_ranks)
-{
-    m_overlap = 400;
-    m_rankset.initialize(DpuProfile{}, "./dpu/short_read_mapping");
-    printf("Using %zu real PIM hardware ranks (%zu DPUs)\n", m_rankset.nb_ranks(), m_rankset.nb_dpu());
-
-    printf("Loading reference\n");
-    graal::Bank reference_bank(reference_path);
-    m_reference = load_and_compress_reference(reference_bank, nb_ranks);
-
-    m_dpu_ref_size = compute_dpu_reference_size(m_reference.seq.size(), m_rankset.nb_dpu(), m_overlap);
-
-    MultiBloomFilter bf = get_bloom_filter(reference_path, m_reference, m_rankset.nb_dpu(), create_bf);
-
-    printf("Building index\n");
-    build_index();
-    m_rankset.wait_all_ranks_done();
-    printf("Index built\n");
-}
-
-void DpuMapper::build_index()
-{
-    index_args.seq_size = m_dpu_ref_size;
-
-    std::vector<uint64_t> dpu_start_pos;
-    dpu_start_pos.reserve(m_rankset.nb_dpu());
-    size_t start_pos = 0;
-    for (PimRankID rank_id = 0; rank_id < m_rankset.nb_ranks(); ++rank_id)
-    {
-        size_t nb_dpu_in_rank = m_rankset.nb_dpu_in_rank(rank_id);
-        std::vector<uint8_t *> buffers(nb_dpu_in_rank);
-        for (size_t dpu_id = 0; dpu_id < nb_dpu_in_rank; ++dpu_id)
-        {
-            buffers[dpu_id] = m_reference.seq.data(start_pos);
-            dpu_start_pos.push_back(start_pos);
-            start_pos = start_pos + m_dpu_ref_size - m_overlap; // Remains a multiple of 4
-        }
-        m_rankset.send_data_to_rank_async<uint8_t>(rank_id, "sequence", 0, buffers,
-                                                   CEILN<8>((m_dpu_ref_size >> 2) * sizeof(uint8_t)));
-        m_rankset.broadcast_to_rank_async(rank_id, "index_args", 0, &index_args, sizeof(index_args));
-        m_rankset.launch_rank_async(rank_id);
-    }
-}
-
-void DpuMapper::map(const std::string &queries_path, const std::string &output_path)
-{
-    const std::array<size_t, 2> &round_shift = {5, 15};
-
-    graal::BankFastaMMap<false> queries_bank(queries_path);
-    auto estimation = queries_bank.estimate();
-    // Retrieve query size (NB: we assume this is the same for all queries)
-    m_seed_search.range = adjust_seed_search_range(estimation.size_mean, round_shift, m_seed_search.range, m_seed_search.delta);
-    // check_read_size(estimation.size_min, estimation.size_max);
-
-    // Get estimated number of queries to reserve memory
-    size_t estimated_nb_queries = estimation.sequences_number;
-
-    std::vector<Mapping> Results;
-
-    printf("Estimation: the bank contains around %lu queries (%lu-(%lu)-%lu bp)\n", estimated_nb_queries,
-           estimation.size_min, estimation.size_mean, estimation.size_max);
-
-    auto it_begin = queries_bank.begin();
-    const auto it_end = queries_bank.end();
-
-    constexpr auto QUERIES_BATCH_SIZE = (1 << 17);
-    std::vector<Read> reads;
-    reads.reserve(QUERIES_BATCH_SIZE);
-    size_t nb_dispatches_r1 = 0;
-
-    ssize_t query_id = 0;
-    while (it_begin != it_end)
-    {
-        const auto &query = reads.emplace_back(*it_begin, query_id);
-        if (query.seq.size() >= min_query_size(m_seed_search.range, m_seed_search.delta))
-        {
-            nb_dispatches_r1 += dispatch_query(query, round_shift[0], &Results);
-        }
-    }
+    return load_bloom_filter(reference_file, nb_dpu, HASH_SIZE);
 }
 
 void post_process_mapping(std::vector<MapResults> rank_map_results, std::vector<MapAllArgs> *args,
@@ -188,16 +80,166 @@ void post_process_mapping(std::vector<MapResults> rank_map_results, std::vector<
     mapping_data->allocator.release(args); // Give the buffer back as available
 }
 
-void launch_mapping(PimRankSet<> &pim_rankset, PimRankID rank_id, std::vector<MapAllArgs> *args, MappingWorkerData *mapping_data)
-{
-    pim_rankset.lock_rank(rank_id);
-    pim_rankset.send_data_to_rank_async<MapAllArgs, MapArgs>(rank_id, "map_args", 0, *args, sizeof(MapArgs));
-    pim_rankset.launch_rank_async(rank_id);
+/* -------------------------------------------------------------------------- */
+/*                               DpuMapper implem                             */
+/* -------------------------------------------------------------------------- */
 
-    pim_rankset.add_callback_async(rank_id, [&pim_rankset, args, rank_id, mapping_data]()
-                                   { mapping_data->pool.push_task(
-                                         post_process_mapping,
-                                         pim_rankset.get_data_from_rank_sync<MapResults>(rank_id, "map_results", 0, sizeof(MapResults)), args,
-                                         pim_rankset.get_rank_start_dpu_id(rank_id), mapping_data); });
-    pim_rankset.unlock_rank(rank_id);
+size_t DpuMapper::dispatch_query(const Read &query, ssize_t shift, MappingWorkerData &mapping_data)
+{
+    size_t nb_dispatches = 0;
+    shift = get_round_shift(shift, query.seq.size(), m_min_query_size);
+    auto start_pos = find_good_pos(query, m_seed_search.range, shift);
+
+    auto start_pos2 = find_good_pos(query, start_pos + m_seed_search.delta, shift);
+    auto signatures = Signature<HASH_SIZE>::hash(query, start_pos, start_pos2);
+    for (auto dpu_id : m_bloom_filters.contains(signatures.first, signatures.second))
+    {
+        dispatch_query_to_dpu(dpu_id, query, start_pos, mapping_data);
+        ++nb_dispatches;
+    }
+
+    return nb_dispatches;
+}
+
+void DpuMapper::dispatch_query_to_dpu(size_t dpu_id, const Read &query, size_t start_pos, MappingWorkerData &mapping_data)
+{
+    auto rank_id = m_rankset.get_rank_id_of_dpu_id(dpu_id);
+    auto i = dpu_id - m_rankset.get_rank_start_dpu_id(rank_id);
+
+    auto &rank_args = mapping_data.rank_args;
+
+    // If no existing buffers for rank, create them
+    if (rank_args[rank_id] == NULL)
+    {
+        while (true)
+        {
+            rank_args[rank_id] = mapping_data.allocator.acquire();
+            if (rank_args[rank_id] != NULL)
+            {
+                break; // Got a buffer
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Got nothing, wait a little bit
+                                                                        // Ranks usually execute in around 5 ms
+        }
+        rank_args[rank_id]->resize(m_rankset.nb_dpu_in_rank(rank_id));
+        for (auto &data : *(rank_args[rank_id]))
+            data.dpu_args.nb_queries = 0;
+    }
+
+    auto &args = rank_args[rank_id];
+    auto &arg = (*args)[i];
+
+    // Add item to args
+    memcpy(arg.dpu_args.queries + (MAX_QUERY_SIZE / 4) * arg.dpu_args.nb_queries, query.seq.data(),
+           query.seq.data_size() * sizeof(uint8_t));
+    arg.dpu_args.seed_positions[arg.dpu_args.nb_queries] = start_pos;
+    auto size = static_cast<uint8_t>(query.seq.size() - 1);
+    arg.dpu_args.query_sizes[arg.dpu_args.nb_queries] = size;
+    arg.identifiers.data[arg.dpu_args.nb_queries] = query.id;
+    arg.identifiers.read_sizes[arg.dpu_args.nb_queries] = size;
+    if (++arg.dpu_args.nb_queries >= MAX_NB_QUERIES_PER_DPU)
+    {
+        // If target buffer become full, launch rank
+        launch_mapping(rank_id, args, &mapping_data);
+        rank_args[rank_id] = NULL;
+    }
+}
+
+DpuMapper::DpuMapper(const std::string &reference_path, ssize_t nb_ranks, bool create_bf) : m_rankset(nb_ranks, nb_ranks)
+{
+    m_overlap = 400;
+    m_rankset.initialize(DpuProfile{}, "./dpu/short_read_mapping");
+    printf("Using %zu real PIM hardware ranks (%zu DPUs)\n", m_rankset.nb_ranks(), m_rankset.nb_dpu());
+
+    printf("Loading reference\n");
+    graal::Bank reference_bank(reference_path);
+    m_reference = load_and_compress_reference(reference_bank, nb_ranks);
+
+    m_dpu_ref_size = compute_dpu_reference_size(m_reference.seq.size(), m_rankset.nb_dpu(), m_overlap);
+
+    m_bloom_filters = get_bloom_filter(reference_path, m_reference, m_rankset.nb_dpu(), create_bf);
+
+    printf("Building index\n");
+    build_index();
+    m_rankset.wait_all_ranks_done();
+    printf("Index built\n");
+}
+
+void DpuMapper::build_index()
+{
+    index_args.seq_size = m_dpu_ref_size;
+
+    m_dpu_start_pos.reserve(m_rankset.nb_dpu());
+    size_t start_pos = 0;
+    for (PimRankID rank_id = 0; rank_id < m_rankset.nb_ranks(); ++rank_id)
+    {
+        size_t nb_dpu_in_rank = m_rankset.nb_dpu_in_rank(rank_id);
+        std::vector<uint8_t *> buffers(nb_dpu_in_rank);
+        for (size_t dpu_id = 0; dpu_id < nb_dpu_in_rank; ++dpu_id)
+        {
+            buffers[dpu_id] = m_reference.seq.data(start_pos);
+            m_dpu_start_pos.push_back(start_pos);
+            start_pos = start_pos + m_dpu_ref_size - m_overlap; // Remains a multiple of 4
+        }
+        m_rankset.send_data_to_rank_async<uint8_t>(rank_id, "sequence", 0, buffers,
+                                                   CEILN<8>((m_dpu_ref_size >> 2) * sizeof(uint8_t)));
+        m_rankset.broadcast_to_rank_async(rank_id, "index_args", 0, &index_args, sizeof(index_args));
+        m_rankset.launch_rank_async(rank_id);
+    }
+}
+
+void DpuMapper::map(const std::string &queries_path, const std::string &output_path)
+{
+    const std::array<ssize_t, 2> round_shift = {5, 15};
+
+    graal::BankFastaMMap<false> queries_bank(queries_path);
+    auto estimation = queries_bank.estimate();
+    // Retrieve query size (NB: we assume this is the same for all queries)
+    m_seed_search.range = adjust_seed_search_range(estimation.size_mean, round_shift, m_seed_search.range, m_seed_search.delta);
+    // check_read_size(estimation.size_min, estimation.size_max);
+    m_min_query_size = min_query_size(m_seed_search.range, m_seed_search.delta);
+
+    // Get estimated number of queries to reserve memory
+    size_t estimated_nb_queries = estimation.sequences_number;
+
+    std::vector<Mapping> results;
+
+    printf("Estimation: the bank contains around %lu queries (%lu-(%lu)-%lu bp)\n", estimated_nb_queries,
+           estimation.size_min, estimation.size_mean, estimation.size_max);
+
+    auto it_begin = queries_bank.begin();
+    const auto it_end = queries_bank.end();
+
+    constexpr auto QUERIES_BATCH_SIZE = (1 << 17);
+    std::vector<Read> reads;
+    reads.reserve(QUERIES_BATCH_SIZE);
+    size_t nb_dispatches_r1 = 0;
+    BS::thread_pool_light result_thread_pool(2); // Two threads are enough, unlikely to have enough work to stall
+
+    MappingWorkerData worker_data(results, static_cast<size_t>(m_rankset.nb_ranks()), result_thread_pool, m_dpu_start_pos);
+
+    ssize_t query_id = 0;
+    while (it_begin != it_end)
+    {
+        const auto &query = reads.emplace_back(*it_begin, query_id);
+        if (query.seq.size() >= static_cast<size_t>(m_min_query_size))
+        {
+            nb_dispatches_r1 += dispatch_query(query, round_shift[0], worker_data);
+        }
+        ++it_begin;
+    }
+}
+
+void DpuMapper::launch_mapping(PimRankID rank_id, std::vector<MapAllArgs> *args, MappingWorkerData *mapping_data)
+{
+    m_rankset.lock_rank(rank_id);
+    m_rankset.send_data_to_rank_async<MapAllArgs, MapArgs>(rank_id, "map_args", 0, *args, sizeof(MapArgs));
+    m_rankset.launch_rank_async(rank_id);
+
+    m_rankset.add_callback_async(rank_id, [this, args, rank_id, mapping_data]()
+                                 { mapping_data->pool.push_task(
+                                       post_process_mapping,
+                                       m_rankset.get_data_from_rank_sync<MapResults>(rank_id, "map_results", 0, sizeof(MapResults)), args,
+                                       m_rankset.get_rank_start_dpu_id(rank_id), mapping_data); });
+    m_rankset.unlock_rank(rank_id);
 }
--- src/dpu_mapper.hpp
+++ src/dpu_mapper.hpp
@@ -3,6 +3,7 @@
 
 #include "pim_rankset.hpp"
 #include "read.hpp"
+#include "read_mapper.hpp"
 
 class DpuMapper
 {
@@ -13,17 +14,25 @@ public:
 
 private:
     void build_index();
+    size_t dispatch_query(const Read &query, ssize_t shift, MappingWorkerData &mapping_data);
+    void dispatch_query_to_dpu(size_t dpu_id, const Read &query, size_t start_pos, MappingWorkerData &mapping_data);
+    void launch_mapping(PimRankID rank_id, std::vector<MapAllArgs> *args, MappingWorkerData *mapping_data);
 
     CompactReference m_reference;
     PimRankSet<> m_rankset;
     ssize_t m_overlap{};
     ssize_t m_dpu_ref_size{};
+    ssize_t m_min_query_size{};
+    ssize_t m_max_query_size{};
     IndexArgs index_args{};
+    std::vector<size_t> m_dpu_start_pos;
+
+    MultiBloomFilter m_bloom_filters;
 
     struct
     {
-        size_t range{15};
-        size_t delta{1};
+        ssize_t range{15};
+        ssize_t delta{1};
     } m_seed_search{};
 };
 
